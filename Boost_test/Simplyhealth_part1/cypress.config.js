4426d73aac7082c99dd02f62ce28f864b2369ebb
jsavantika <128582663+jsavantika@users.noreply.github.com>
2023-06-15 12:22:39 +0100

    .

--- Boost_test/Simplyhealth_part1/cypress.config.js
+++ Boost_test/Simplyhealth_part1/cypress.config.js
@@ -1,7 +1,8 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-
+	 userAgent:
+    'Chrome/114.0.5735.111',
     e2e: {
     
         baseUrl: 'https://www.simplyhealth.co.uk'
